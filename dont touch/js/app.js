f8c6dfd9fa0150557b17f8b9147ff93159f6a1c7
Artemndreevich <45665459+Artemndreevich@users.noreply.github.com>
2019-07-20 10:42:49 +0300

    1

--- dont touch/js/app.js
+++ dont touch/js/app.js
@@ -7,11 +7,14 @@ $('.tlf-img').click(function () {
     $('.helpToTlf').fadeIn(300);
     $('.tlfMenuContainer').css('background-color','white');
 
+    $('.tlf-log  ').css('background-color','white');
+
 
 });
     $('#any-div').click(function () {
         $('.helpToTlf').fadeOut(200);
         $('.tlfMenuContainer').css('background-color','transparent');
+        $('.tlf-log  ').css('background-color','transparent');
 
     });
 
